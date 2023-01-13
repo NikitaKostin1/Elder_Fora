79d3bfc0cff1eebcea710d79ea92ebbbd24939e5
NikitaKostin1 <nikitos9278@gmail.com>
2023-01-13 18:53:11 +0200

    UPDATE: ai/connection engine

--- ai/connection.py
+++ ai/connection.py
@@ -12,12 +12,12 @@ async def get_response(prompt: str) -> str:
 		data = {
 			"prompt": prompt,
 			"temperature": 0.5,
-			"max_tokens": 3400,
+			"max_tokens": 1800,
 			"top_p": 1.0,
 			"frequency_penalty": 0.5,
 			"presence_penalty": 0.0
 		}
-		url = f"{openai.api_url}/v1/engines/text-davinci-002/completions"
+		url = f"{openai.api_url}/v1/engines/text-davinci-003/completions"
 
 		async with session.post(url, json=data, headers=headers) as resp:
 			json_response = await resp.json()
